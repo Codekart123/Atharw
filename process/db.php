8d2203dfff665e311b330cb2f85b9ce07e0d6ccb
bhagyashree0654 <bhagyashreepatra004@gmail.com>
2021-09-06 16:27:18 +0530

    Update db.php

--- process/db.php
+++ process/db.php
@@ -2,12 +2,16 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
-$database = "atharw";
+$database = "athraw";
 
 // Create connection
 $conn = mysqli_connect($servername, $username, $password, $database);
 
 // Check connection
 if (!$conn) {
+    echo "ERROR";
     die("Connection failed: " . mysqli_connect_error());
+}
+else{
+    echo "Connected..";
 }
